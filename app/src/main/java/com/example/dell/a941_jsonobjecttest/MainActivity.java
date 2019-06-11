79fbc4b40e2fc3251307188b2220881b68c57b36
PengShaofeng <3274642663@qq.com>
2019-06-11 11:57:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/a941_jsonobjecttest/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.dell.a941_jsonobjecttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView responseText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.send_request) {
+            sendRequestWithOkHttp();
+        }
+    }
+    private  void sendRequestWithOkHttp(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("https://10.0.2.2/get_data.json")
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    parseJSONWithJSONObject(responseData);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    private void parseJSONWithJSONObject(String jsonData){
+        try{
+            JSONArray jsonArray = new JSONArray(jsonData);
+            for (int i=0; i< jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                String name = jsonObject.getString("name");
+                String version = jsonObject.getString("version");
+                Log.d("MainActivity","id is " + id);
+                Log.d("MainActivity","name is " + name);
+                Log.d("MainActivity","version is " + version);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
